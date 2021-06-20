89b79774ee8a55f1d0c8b45c886abb2ef5e98bc1
Nicolas Fortun <nicolas.fortun.nageotte@gmail.com>
2021-06-20 15:01:37 +0200

    Remove infinite loop

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Python script
 
 Check resistive moisture sensor periodically. If no input is found, it sends a mail to alert the owner that the sensor is dry.
+You can use a cron to trigger the script periodically
--- moisture_sensor.py
+++ moisture_sensor.py
@@ -3,27 +3,38 @@
 import RPi.GPIO as GPIO
 import time
 import send_mail as mail
+from dotenv import load_dotenv, set_key
+import os
 
+SENSOR_ENV_KEY='SENSOR_STATE'
+ENV_PATH='/home/pi/Python/.env'
+load_dotenv(ENV_PATH)
 channel=17
-previousState=-1
+
+if os.getenv(SENSOR_ENV_KEY) == None:
+    previousState=-1
+else:
+    previousState= int(os.environ[SENSOR_ENV_KEY])
+    print(previousState)
 input=4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 GPIO.setup(input, GPIO.OUT)
 
-
-while True:
-    print("Lecture de la sonde à : "+ time.strftime("%H:%M:%S", time.localtime()))
-    GPIO.output(input, GPIO.HIGH)
-    if GPIO.input(channel) == previousState:
-        print("Aucun changement")
-    elif GPIO.input(channel):
-        print("Il n'y a plus d'eau")
-        mail.send_email("Il n'y a plus d'eau, il faut arroser le bonsai")
-    else:
-        print("Le bonsai est hydraté")
-        mail.send_email("Le bonsai est bien hydrate !")
-    previousState = GPIO.input(channel)
-    GPIO.output(input, GPIO.LOW)
-    time.sleep(3600)
+print("Lecture de la sonde à : "+ time.strftime("%H:%M:%S", time.localtime()))
+GPIO.output(input, GPIO.HIGH)
+time.sleep(1)
+currentState=GPIO.input(channel)
+if currentState == previousState:
+    print("Aucun changement")
+elif currentState:
+    print("Il n'y a plus d'eau")
+    set_key(ENV_PATH, SENSOR_ENV_KEY, str(currentState))
+    mail.send_email("Il n'y a plus d'eau, il faut arroser le bonsai")
+else:
+    print("Le bonsai est hydraté")
+    set_key(ENV_PATH, SENSOR_ENV_KEY, str(currentState))
+    mail.send_email("Le bonsai est bien hydrate !")
+GPIO.output(input, GPIO.LOW)
+GPIO.cleanup()
 
